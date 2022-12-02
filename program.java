c245fcc7b12fde95df33937b384d184d1990a436
Viktoria <viktoriapredko1@gmail.com>
2022-12-02 12:25:07 +0300

    Solve tasks

---
+++ program.java
@@ -0,0 +1,66 @@
+// 1.Написать метод, который переведёт данное целое число в римский формат.
+public class program{
+    private static int[] intervals={0, 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    private static String[] numerals={"", "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+    
+    public static void main(String args[]) {
+        int i=1001;
+        String roman=toRoman(i);
+        
+        System.out.println(roman);
+    }
+    
+     
+     
+    private final static int findFloor(final int number, final int firstIndex, final int lastIndex) {
+        if(firstIndex==lastIndex)
+            return firstIndex;
+        if(intervals[firstIndex]==number)
+            return firstIndex;
+        if(intervals[lastIndex]==number)
+            return lastIndex;
+        final int median=(lastIndex+firstIndex)/2;
+        if(median==firstIndex)
+            return firstIndex;
+        if(number == intervals[median])
+            return median;
+        if(number > intervals[median])
+            return findFloor(number, median, lastIndex);
+        else
+            return findFloor(number, firstIndex, median);
+
+    }
+
+    public final static String toRoman(final int number) {
+        int floorIndex=findFloor(number, 0, intervals.length-1);
+        if(number==intervals[floorIndex]){
+            return numerals[floorIndex]; 
+        }
+        return numerals[floorIndex]+toRoman(number-intervals[floorIndex]);
+    }
+    
+}
+
+// 2.Написать метод, который переведет число из римского формата записи в арабский.
+public class program {
+    private static int[] intervals={0, 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    private static String[] numerals={"", "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+    
+    public static void main(String args[]) {
+        
+        String roman="MXC";
+        int arabic = toArabic(roman);
+        System.out.println(arabic);
+    }
+    
+    public static int toArabic(String roman) {
+        int result = 0;
+        for (int i = intervals.length-1; i >= 0; i-- ) {
+            while (roman.indexOf(numerals[i]) == 0 && numerals[i].length() > 0) {
+                result += intervals[i];
+                roman = roman.substring(numerals[i].length());
+            }
+        }
+        return result;
+    }
+}
